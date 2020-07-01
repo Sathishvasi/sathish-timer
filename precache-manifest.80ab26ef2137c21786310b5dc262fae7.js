9358f78915a5cf8caf1d650e0a4633ec26ba31e6
sathishvasi <sathish@wizroots.com>
2020-07-01 21:31:15 +0530

    Initial commit

---
+++ precache-manifest.80ab26ef2137c21786310b5dc262fae7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702193d6e52d254eb1ec2fca71d64e30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43b4fe75d6f612d3251c",
+    "url": "/static/css/main.68a952c6.chunk.css"
+  },
+  {
+    "revision": "a7754a4236ebdf1b5572",
+    "url": "/static/js/2.aa2d56f2.chunk.js"
+  },
+  {
+    "revision": "bc23c04eec3c55de9f1e7db1db146add",
+    "url": "/static/js/2.aa2d56f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43b4fe75d6f612d3251c",
+    "url": "/static/js/main.8ac75154.chunk.js"
+  },
+  {
+    "revision": "e2d3924312a67be16390",
+    "url": "/static/js/runtime-main.9043f974.js"
+  },
+  {
+    "revision": "77eb3f0775a5dceec1796e5937560c63",
+    "url": "/static/media/background.77eb3f07.jpg"
+  },
+  {
+    "revision": "95a5ac28168f38e2b6991e195f90caea",
+    "url": "/static/media/banner-completed.95a5ac28.png"
+  },
+  {
+    "revision": "293a4c7bde3fb34cabe64eb9d477c36b",
+    "url": "/static/media/banner-img.293a4c7b.png"
+  },
+  {
+    "revision": "8fe5331de89798c86b4932f81fe96415",
+    "url": "/static/media/demo.8fe5331d.png"
+  }
+]);
